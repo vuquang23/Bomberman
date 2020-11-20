0a44f10fa3bc8c4d8da5fbfdccfa4cc208f1fc3b
JLuckyBoyJ <bachb0952001@gmail.com>
2020-11-20 22:09:18 +0700

    Balloon move + Pic

--- src/uet/oop/bomberman/entities/Balloon.java
+++ src/uet/oop/bomberman/entities/Balloon.java
@@ -1,12 +1,28 @@
 package uet.oop.bomberman.entities;
 
 import javafx.scene.image.Image;
+import uet.oop.bomberman.graphics.Sprite;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Balloon extends Enemy {
     public Balloon(int x, int y, Image img) {
         super( x, y, img);
+        //up
+        constImage.add(new ArrayList<Image>());
+        //right
+        constImage.add(new ArrayList<Image>());
+        constImage.get(1).add(Sprite.balloom_right1.getFxImage());
+        constImage.get(1).add(Sprite.balloom_right2.getFxImage());
+        constImage.get(1).add(Sprite.balloom_right3.getFxImage());
+        //down
+        constImage.add(new ArrayList<Image>());
+        //left
+        constImage.add(new ArrayList<Image>());
+        constImage.get(3).add(Sprite.balloom_left1.getFxImage());
+        constImage.get(3).add(Sprite.balloom_left2.getFxImage());
+        constImage.get(3).add(Sprite.balloom_left3.getFxImage());
     }
     static Random ran = new Random();
     static int[] xx = {0, 0, -4, 4};
@@ -17,5 +33,14 @@ public class Balloon extends Enemy {
         if (nextDir < 0) nextDir += 4;
         this.addX(xx[nextDir]);
         this.addY(yy[nextDir]);
+        curState += 1;
+        curState %= 3;
+        if (nextDir == 1) {
+            this.img = constImage.get(nextDir).get(curState);
+        } else {
+            if (nextDir == 3) {
+                this.img = constImage.get(nextDir).get(curState);
+            }
+        }
     }
 }
